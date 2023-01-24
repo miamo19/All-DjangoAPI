712a16976dd5b60455f8d189a9031ff047b07991
Hyacinthe <hyacinthemiamo5@gmail.com>
2023-01-24 05:39:28 +0100

    Update views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,9 @@
+#from django
 from django.http import JsonResponse
+
 import json
 
-#Create your views here.
+
 def api_home(request, *args, **kwargs):
     body = request.body  #byte string of JSON data
     print(type(body))
