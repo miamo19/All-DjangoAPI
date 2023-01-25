61b281dea7d3e5654364f9c9c75cdd854fe81f30
Hyacinthe <hyacinthemiamo5@gmail.com>
2023-01-25 03:44:12 +0100

    Update basic.py

--- client_app/basic.py
+++ client_app/basic.py
@@ -1,7 +1,7 @@
 import requests
 
 endpoint = "http://localhost:8000/"
-response = requests.get(endpoint, params={"name": "Hyacinth"}, json={"MATRI":"UBA19E0181"})   #params={"abc": 123},
+response = requests.get(endpoint, params={"name": "Hyacinth"}, json={"MATRI":"UBA19E0181"})   
 # print(response.text)
 # print(response.status_code)
 print(response.json())
