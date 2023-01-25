c766423cf5c85efd8456c544ce2fe34970979902
Hyacinthe <hyacinthemiamo5@gmail.com>
2023-01-25 03:43:11 +0100

    Update urls.py

--- backend/miacode/urls.py
+++ backend/miacode/urls.py
@@ -1,9 +1,9 @@
-
+#from django
 from django.contrib import admin
 from django.urls import path,include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('api.urls'))
+    path('',include('api.urls')),
 ]
